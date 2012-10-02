5c668cda8655b3e05b775ed8fa6fc9535278790f
Maarten ter Huurne <maarten@treewalker.org>
2012-10-02 22:38:15 +0200

    MIPS: JZ4770: Board support for GCW Zero
    This is a squashed version of development done in the jz-3.18-dts branch.

---
+++ arch/mips/include/asm/mach-jz4770/board-gcw0.h
@@ -0,0 +1,22 @@
+/*
+ * board-gcw0.h  -  GCW Zero: JZ4770-based handheld game console
+ *
+ * File based on Pisces board definition.
+ * Copyright (C) 2008 Ingenic Semiconductor Inc.
+ * Original author: <cwjia@ingenic.cn>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef __ASM_JZ4770_GCW0_H__
+#define __ASM_JZ4770_GCW0_H__
+
+/*======================================================================
+ * Frequencies of on-board oscillators
+ */
+#define JZ_EXTAL		12000000  /* Main extal freq:	12 MHz */
+#define JZ_EXTAL2		32768     /* RTC extal freq:	32.768 KHz */
+
+#endif /* __ASM_JZ4770_GCW0_H__ */
