24c46acccad9b469501f655c33312bca6171e788
Maarten ter Huurne <maarten@treewalker.org>
2013-09-02 15:16:39 +0200

    gpu: vivante: Moved away options that are no longer inspected in uapi
    After the uapi minimization, 9 out of 14 options used by uapi headers
    are no longer used there, so move those option definitions to the
    internal options header.

--- drivers/gpu/vivante/v4/hal/kernel/gc_hal_options.h
+++ drivers/gpu/vivante/v4/hal/kernel/gc_hal_options.h
@@ -27,6 +27,15 @@
 #include "gc_hal_options_to_be_removed.h"
 
 /*
+    gcdPRINT_VERSION
+
+        Print HAL version.
+*/
+#ifndef gcdPRINT_VERSION
+#   define gcdPRINT_VERSION                     0
+#endif
+
+/*
     NO_USER_DIRECT_ACCESS_FROM_KERNEL
 
         This define enables the Linux kernel behavior accessing user memory.
@@ -36,11 +45,53 @@
 #endif
 
 /*
+    PROFILE_HAL_COUNTERS
+
+        This define enables HAL counter profiling support.  HW and SHADER
+        counter profiling depends on this.
+*/
+#ifndef PROFILE_HAL_COUNTERS
+#   define PROFILE_HAL_COUNTERS                 1
+#endif
+
+/*
     COMMAND_PROCESSOR_VERSION
 
         The version of the command buffer and task manager.
 */
 #define COMMAND_PROCESSOR_VERSION               1
+
+/*
+    gcdDUMP_API
+
+        When set to 1, a high level dump of the EGL and GL/VG APs's are
+        captured.
+*/
+#ifndef gcdDUMP_API
+#   define gcdDUMP_API                          0
+#endif
+
+/*
+    gcdDUMP_FRAMERATE
+        When set to a value other than zero, averaqe frame rate will be dumped.
+        The value set is the starting frame that the average will be calculated.
+        This is needed because sometimes first few frames are too slow to be included
+        in the average. Frame count starts from 1.
+*/
+#ifndef gcdDUMP_FRAMERATE
+#   define gcdDUMP_FRAMERATE					0
+#endif
+
+/*
+    gcdDUMP_COMMAND
+
+        When set to non-zero, the command queue will dump all incoming command
+        and context buffers as well as all other modifications to the command
+        queue.
+*/
+#ifndef gcdDUMP_COMMAND
+#   define gcdDUMP_COMMAND                      0
+#endif
 
 /*
     gcdNULL_DRIVER
@@ -169,6 +220,18 @@
 #endif
 
 /*
+    gcdENABLE_BANK_ALIGNMENT
+
+    When enabled, video memory is allocated bank aligned. The vendor can modify
+    _GetSurfaceBankAlignment() and gcoSURF_GetBankOffsetBytes() to define how
+    different types of allocations are bank and channel aligned.
+    When disabled (default), no bank alignment is done.
+*/
+#ifndef gcdENABLE_BANK_ALIGNMENT
+#   define gcdENABLE_BANK_ALIGNMENT             0
+#endif
+
+/*
     gcdBANK_BIT_START
 
     Specifies the start bit of the bank (inclusive).
@@ -220,6 +283,15 @@
 */
 #ifndef gcdDYNAMIC_EVENT_THRESHOLD
 #    define gcdDYNAMIC_EVENT_THRESHOLD          5
+#endif
+
+/*
+    gcdENABLE_PROFILING
+
+        Enable profiling macros.
+*/
+#ifndef gcdENABLE_PROFILING
+#   define gcdENABLE_PROFILING                  0
 #endif
 
 /*
@@ -292,12 +364,32 @@
 #endif
 
 /*
+    gcdUSE_VIDMEM_PER_PID
+*/
+#ifndef gcdUSE_VIDMEM_PER_PID
+#   define gcdUSE_VIDMEM_PER_PID                0
+#endif
+
+/*
     gcdENABLE_RECOVERY
 
         This define enables the recovery code.
 */
 #ifndef gcdENABLE_RECOVERY
 #   define gcdENABLE_RECOVERY                   1
+#endif
+
+/*
+    gcdSMP
+
+        This define enables SMP support.
+
+        Kbuild will config it according to whether
+        CONFIG_SMP is set.
+
+*/
+#ifndef gcdSMP
+#   define gcdSMP                               0
 #endif
 
 /*
