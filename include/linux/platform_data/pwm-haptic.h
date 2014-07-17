55e4820465553e30b3fe2614f1186ef08a81e932
Paul Cercueil <paul@crapouillou.net>
2014-07-17 14:29:28 +0200

    input: misc: Added driver pwm-haptic
    This driver can be used to create an haptic device that controls a motor
    connected to a PWM output.

---
+++ include/linux/platform_data/pwm-haptic.h
@@ -0,0 +1,22 @@
+/*
+ * Driver for PWM based haptic devices
+ *
+ * Copyright (C) 2014 Paul Cercueil <paul@crapouillou.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef __PLATFORM_DATA_PWM_HAPTIC_H__
+#define __PLATFORM_DATA_PWM_HAPTIC_H__
+
+#include <linux/pwm.h>
+
+struct pwm_haptic_platform_data {
+	unsigned int pwm_id;
+	enum pwm_polarity polarity;
+	unsigned long pwm_period_ns;
+};
+
+#endif /* __PLATFORM_DATA_PWM_HAPTIC_H__ */
