fb6d5054573016aa89b7fa24c6b0ed28a2198033
Maarten ter Huurne <maarten@treewalker.org>
2014-07-19 22:42:20 +0200

    opendingux: Adapt modules fs creation script to new naming

--- create_modules_fs.sh
+++ create_modules_fs.sh
@@ -8,12 +8,12 @@ mkdir /tmp/modules_fs
 INSTALL_MOD_PATH=/tmp/modules_fs ARCH=mips make modules_install
 
 NAME=`ls -d /tmp/modules_fs/lib/modules/*`
-NAME=`basename "$NAME" -dirty`
+NAME=`basename "$NAME" +`
 
 if [ -d  "/tmp/modules_fs/lib/modules/${NAME}" ] ; then
-	ln -s "/lib/modules/${NAME}" "/tmp/modules_fs/lib/modules/${NAME}-dirty"
+	ln -s "${NAME}" "/tmp/modules_fs/lib/modules/${NAME}+"
 else
-	ln -s "/lib/modules/${NAME}-dirty" "/tmp/modules_fs/lib/modules/${NAME}"
+	ln -s "${NAME}+" "/tmp/modules_fs/lib/modules/${NAME}"
 fi
 
 mksquashfs /tmp/modules_fs/lib/modules modules.squashfs -all-root -noappend -no-exports -no-xattrs
