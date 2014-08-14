085d37df22518dc9cbcf1d425397ae651183b383
Paul Cercueil <paul@crapouillou.net>
2014-08-14 21:49:38 +0200

    input: pwm-haptic: Properly configure PWM when opening / closing device
    It was previously incorrectly configuring the PWM, which resulted in the
    haptic device running at full power as soon as the device node was
    opened.

--- drivers/input/misc/pwm-haptic.c
+++ drivers/input/misc/pwm-haptic.c
@@ -62,7 +62,7 @@ static int pwm_haptic_open(struct input_dev *input)
 		return -EBUSY;
 
 	haptic->is_open = true;
-	pwm_config(haptic->pwm, 0, haptic->pwm_period);
+	pwm_config(haptic->pwm, haptic->pwm_period, haptic->pwm_period);
 	pwm_set_polarity(haptic->pwm, haptic->polarity);
 	pwm_enable(haptic->pwm);
 	return 0;
@@ -71,7 +71,7 @@ static int pwm_haptic_open(struct input_dev *input)
 static void pwm_haptic_close(struct input_dev *input)
 {
 	struct pwm_haptic *haptic = input_get_drvdata(input);
-	pwm_config(haptic->pwm, 0, haptic->pwm_period);
+	pwm_config(haptic->pwm, haptic->pwm_period, haptic->pwm_period);
 	pwm_disable(haptic->pwm);
 	haptic->is_open = false;
 }
