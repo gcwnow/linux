1bcc1eca38f4b08323d6808fee8b3ecc4478f73f
Maarten ter Huurne <maarten@treewalker.org>
2014-09-06 08:42:06 +0200

    MIPS: JZ4770: GCW0: Correct polarity of backlight PWM
    The backlight is high active (verified using GPIO mode), so it should
    be using normal polarity. The inversed polarity configuration used to
    work because the PWM driver had a bug that flipped polarity, but that
    bug has now been fixed.

--- arch/mips/jz4770/board-gcw0.c
+++ arch/mips/jz4770/board-gcw0.c
@@ -503,7 +503,7 @@ static struct platform_device gcw0_i2c4_gpio_device = {
 /* LCD backlight */
 
 static struct platform_pwm_backlight_data gcw0_backlight_pdata = {
-	.polarity = PWM_POLARITY_INVERSED,
+	.polarity = PWM_POLARITY_NORMAL,
 	.max_brightness = 255,
 	.dft_brightness = 145,
 	.pwm_period_ns = 40000, /* 25 kHz: outside human hearing range */
