8d0fa5e78ca18f21d2b41444900d0f9080780c42
Maarten ter Huurne <maarten@treewalker.org>
2014-10-10 14:32:42 +0200

    i2c: Remove JZ4770 I2C driver from Ingenic
    We are using the i2c-gpio driver instead. If we want to make use of the
    hardware I2C support, we can switch to the JZ4780 driver once we're on
    a shared source tree, or modernize this driver to work with DT.

--- include/linux/platform_data/jz4770_i2c.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PLATFORM_DATA_JZ4770_I2C_H__
-#define __PLATFORM_DATA_JZ4770_I2C_H__
-
-struct i2c_jz4770_platform_data {
-	bool	use_dma;
-};
-
-#endif /* __PLATFORM_DATA_JZ4770_I2C_H__ */
