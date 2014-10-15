1e2ad083b41dca1b57685e3dd4162b92a2816e7b
Maarten ter Huurne <maarten@treewalker.org>
2014-10-15 03:24:05 +0200

    MIPS: JZ4770: GCW0: Power panel on/off via platform-lcd driver
    We used to have a custom panel interface for this, but platform-lcd
    offers similar functionality in a looser coupled way.

--- arch/mips/jz4770/board-gcw0.c
+++ arch/mips/jz4770/board-gcw0.c
@@ -36,7 +36,6 @@
 
 #include <linux/mmc/host.h>
 #include <linux/act8600_power.h>
-#include <linux/platform_data/jz4770_fb.h>
 #include <linux/platform_data/jz4770_mmc.h>
 #include <linux/platform_data/linkdev.h>
 #include <linux/platform_data/mxc6225.h>
@@ -53,6 +52,7 @@
 #include <sound/jz4770.h>
 #include <video/jzpanel.h>
 #include <video/panel-nt39016.h>
+#include <video/platform_lcd.h>
 
 #include <asm/mach-jz4770/board-gcw0.h>
 #include <asm/mach-jz4770/gpio.h>
@@ -60,70 +60,6 @@
 #include <asm/mach-jz4770/platform.h>
 
 #include "clock.h"
-
-
-/* Video */
-
-#define GPIO_PANEL_SOMETHING	JZ_GPIO_PORTF(0)
-
-static int gcw0_panel_init(void **out_panel,
-				     struct device *dev, void *panel_pdata)
-{
-	int ret;
-
-	ret = nt39016_panel_ops.init(out_panel, dev, panel_pdata);
-	if (ret)
-		return ret;
-
-	ret = devm_gpio_request(dev, GPIO_PANEL_SOMETHING, "LCD panel unknown");
-	if (ret) {
-		dev_err(dev,
-			"Failed to request LCD panel unknown pin: %d\n", ret);
-		return ret;
-	}
-
-	gpio_direction_output(GPIO_PANEL_SOMETHING, 1);
-
-	return 0;
-}
-
-static void gcw0_panel_exit(void *panel)
-{
-	nt39016_panel_ops.exit(panel);
-}
-
-static void gcw0_panel_enable(void *panel)
-{
-	act8600_output_enable(6, true);
-
-	nt39016_panel_ops.enable(panel);
-}
-
-static void gcw0_panel_disable(void *panel)
-{
-	nt39016_panel_ops.disable(panel);
-
-	act8600_output_enable(6, false);
-}
-
-static struct nt39016_platform_data gcw0_panel_pdata = {
-	.gpio_reset		= JZ_GPIO_PORTE(2),
-	.gpio_clock		= JZ_GPIO_PORTE(15),
-	.gpio_enable		= JZ_GPIO_PORTE(16),
-	.gpio_data		= JZ_GPIO_PORTE(17),
-};
-
-static struct panel_ops gcw0_panel_ops = {
-	.init		= gcw0_panel_init,
-	.exit		= gcw0_panel_exit,
-	.enable		= gcw0_panel_enable,
-	.disable	= gcw0_panel_disable,
-};
-
-static struct jzfb_platform_data gcw0_fb_pdata = {
-	.panel_ops		= &gcw0_panel_ops,
-	.panel_pdata		= &gcw0_panel_pdata,
-};
 
 
 /* Buttons */
@@ -489,6 +425,67 @@ static struct platform_device gcw0_backlight_device = {
 };
 
 
+/* LCD panel */
+
+static struct platform_device gcw0_lcd_device;
+static void *gcw0_lcd_panel;
+
+static struct nt39016_platform_data gcw0_panel_pdata = {
+	.gpio_reset		= JZ_GPIO_PORTE(2),
+	.gpio_clock		= JZ_GPIO_PORTE(15),
+	.gpio_enable		= JZ_GPIO_PORTE(16),
+	.gpio_data		= JZ_GPIO_PORTE(17),
+};
+
+#define GPIO_PANEL_SOMETHING	JZ_GPIO_PORTF(0)
+
+static int gcw0_lcd_probe(struct plat_lcd_data *pdata)
+{
+	int ret;
+
+	struct device *dev = &gcw0_lcd_device.dev;
+
+	ret = nt39016_panel_ops.init(&gcw0_lcd_panel, dev, &gcw0_panel_pdata);
+	if (ret)
+		return ret;
+
+	ret = devm_gpio_request(dev, GPIO_PANEL_SOMETHING, "LCD panel unknown");
+	if (ret) {
+		dev_err(dev,
+			"Failed to request LCD panel unknown pin: %d\n", ret);
+		return ret;
+	}
+
+	gpio_direction_output(GPIO_PANEL_SOMETHING, 1);
+
+	return 0;
+}
+
+static void gcw0_lcd_set_power(struct plat_lcd_data *pdata, unsigned int power)
+{
+	if (power) {
+		act8600_output_enable(6, true);
+		nt39016_panel_ops.enable(gcw0_lcd_panel);
+	} else {
+		nt39016_panel_ops.disable(gcw0_lcd_panel);
+		act8600_output_enable(6, false);
+	}
+}
+
+static struct plat_lcd_data gcw0_lcd_pdata = {
+	.probe = gcw0_lcd_probe,
+	.set_power = gcw0_lcd_set_power,
+};
+
+static struct platform_device gcw0_lcd_device = {
+	.name = "platform-lcd",
+	.dev = {
+		.platform_data = &gcw0_lcd_pdata,
+		.parent = &jz4770_lcd_device.dev,
+	},
+};
+
+
 /* Audio */
 
 static struct jz4770_icdc_platform_data gcw0_icdc_pdata = {
@@ -711,6 +708,7 @@ static struct platform_device *jz_platform_devices[] __initdata = {
 	&jz4770_rtc_device,
 	&gcw0_gpio_keys_device,
 	&gcw0_backlight_device,
+	&gcw0_lcd_device,
 	&gcw0_audio_device,
 	&jz4770_msc0_device,
 	&jz4770_msc1_device,
@@ -730,7 +728,6 @@ static int __init gcw0_init_platform_devices(void)
 			jz4770_usb_otg_device.dev.platform_data;
 	otg_platform_data->board_data = &gcw0_otg_board_data;
 
-	jz4770_lcd_device.dev.platform_data = &gcw0_fb_pdata;
 	jz4770_adc_device.dev.platform_data = &gcw0_battery_pdata;
 	jz4770_msc0_device.dev.platform_data = &gcw_internal_sd_data;
 	jz4770_msc1_device.dev.platform_data = &gcw_external_sd_data;
