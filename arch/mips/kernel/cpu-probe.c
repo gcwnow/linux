3bd95f870fbb75735ef5ff8cffb34b1cdee43c7e
Paul Burton <paul.burton@imgtec.com>
2015-01-18 14:42:47 -0800

    MIPS: add jz4780 Ingenic vendor ID
    With the jz4780, Ingenic switched to use a different vendor ID in the
    coprocessor 0 PRID register (whilst keeping the product ID & revision
    the same as the jz4740 & jz4770...). Add a definition for the new
    vendor ID & handle it in the same way as the older one.
    
    Signed-off-by: Paul Burton <paul.burton@imgtec.com>
    Cc: Lars-Peter Clausen <lars@metafoo.de>

--- arch/mips/kernel/cpu-probe.c
+++ arch/mips/kernel/cpu-probe.c
@@ -1313,6 +1313,7 @@ void cpu_probe(void)
 		cpu_probe_cavium(c, cpu);
 		break;
 	case PRID_COMP_INGENIC:
+	case PRID_COMP_INGENIC_4780:
 		cpu_probe_ingenic(c, cpu);
 		break;
 	case PRID_COMP_NETLOGIC:
