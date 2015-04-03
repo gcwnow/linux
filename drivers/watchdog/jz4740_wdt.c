4da1069ba582971d06d45b468caf7027c2b53442
Paul Cercueil <paul@crapouillou.net>
2015-04-04 01:09:50 +0200

    watchdog: JZ4740: Register a restart handler
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>
    
    (squashed: Unregister the restart handler when removing module)

--- drivers/watchdog/jz4740_wdt.c
+++ drivers/watchdog/jz4740_wdt.c
@@ -25,6 +25,7 @@
 #include <linux/slab.h>
 #include <linux/err.h>
 #include <linux/of.h>
+#include <linux/reboot.h>
 
 #include <asm/mach-jz4740/timer.h>
 
@@ -61,6 +62,8 @@ MODULE_PARM_DESC(heartbeat,
 		"Watchdog heartbeat period in seconds from 1 to "
 		__MODULE_STRING(MAX_HEARTBEAT) ", default "
 		__MODULE_STRING(DEFAULT_HEARTBEAT));
+
+static struct watchdog_device *jz4740_wdt;
 
 struct jz4740_wdt_drvdata {
 	struct watchdog_device wdt;
@@ -130,6 +133,19 @@ static int jz4740_wdt_stop(struct watchdog_device *wdt_dev)
 	return 0;
 }
 
+static int jz4740_wdt_restart_handler(struct notifier_block *nb,
+		unsigned long mode, void *cmd)
+{
+	jz4740_wdt->timeout = 0;
+	jz4740_wdt_start(jz4740_wdt);
+	return NOTIFY_DONE;
+}
+
+static struct notifier_block jz4740_wdt_restart_nb = {
+	.notifier_call = jz4740_wdt_restart_handler,
+	.priority = 128,
+};
+
 static const struct watchdog_info jz4740_wdt_info = {
 	.options = WDIOF_SETTIMEOUT | WDIOF_KEEPALIVEPING | WDIOF_MAGICCLOSE,
 	.identity = "jz4740 Watchdog",
@@ -154,7 +170,6 @@ MODULE_DEVICE_TABLE(of, jz4740_wdt_of_matches)
 static int jz4740_wdt_probe(struct platform_device *pdev)
 {
 	struct jz4740_wdt_drvdata *drvdata;
-	struct watchdog_device *jz4740_wdt;
 	struct resource	*res;
 	int ret;
 
@@ -192,13 +207,21 @@ static int jz4740_wdt_probe(struct platform_device *pdev)
 		goto err_out;
 	}
 
+	ret = register_restart_handler(&jz4740_wdt_restart_nb);
+	if (ret < 0) {
+		dev_err(&pdev->dev, "cannot register restart handler\n");
+		goto err_disable_clk;
+	}
+
 	ret = watchdog_register_device(&drvdata->wdt);
 	if (ret < 0)
-		goto err_disable_clk;
+		goto err_unregister_restart_handler;
 
 	platform_set_drvdata(pdev, drvdata);
 	return 0;
 
+err_unregister_restart_handler:
+	unregister_restart_handler(&jz4740_wdt_restart_nb);
 err_disable_clk:
 	clk_put(drvdata->rtc_clk);
 err_out:
@@ -211,6 +234,7 @@ static int jz4740_wdt_remove(struct platform_device *pdev)
 
 	jz4740_wdt_stop(&drvdata->wdt);
 	watchdog_unregister_device(&drvdata->wdt);
+	unregister_restart_handler(&jz4740_wdt_restart_nb);
 	clk_put(drvdata->rtc_clk);
 
 	return 0;
