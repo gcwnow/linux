661f43bafd441b92767325ee952e8f730108eae9
Maarten ter Huurne <maarten@treewalker.org>
2015-04-14 08:09:41 +0200

    net: wireless: rtl8188eu: Only support Android-specific ioctls on Android
    There is no point in including this code on plain Linux.

--- drivers/net/wireless/rtl8188eu/os_dep/linux/ioctl_linux.c
+++ drivers/net/wireless/rtl8188eu/os_dep/linux/ioctl_linux.c
@@ -13006,9 +13006,11 @@ int rtw_ioctl(struct net_device *dev, struct ifreq *rq, int cmd)
 		case SIOCDEVPRIVATE:
 			ret = rtw_ioctl_wext_private(dev, &wrq->u);
 			break;
+#ifdef CONFIG_ANDROID
 		case (SIOCDEVPRIVATE+1):
 			ret = rtw_android_priv_cmd(dev, rq, cmd);
 			break;
+#endif
 		default:
 			ret = -EOPNOTSUPP;
 			break;
