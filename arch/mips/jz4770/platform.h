5244c1965b29dbf2178df2c79e2efb87fcd10756
Paul Cercueil <paul@crapouillou.net>
2015-05-02 12:25:10 +0200

    MIPS: JZ4770: Remove dead code
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- arch/mips/jz4770/platform.h
+++ arch/mips/jz4770/platform.h
@@ -4,17 +4,10 @@
 #include <linux/platform_device.h>
 
 extern struct platform_device jz4770_adc_device;
-extern struct platform_device jz4770_i2c0_device;
-extern struct platform_device jz4770_i2c1_device;
-extern struct platform_device jz4770_i2c2_device;
 extern struct platform_device jz4770_i2s_device;
 extern struct platform_device jz4770_icdc_device;
 extern struct platform_device jz4770_lcd_device;
-extern struct platform_device jz4770_msc0_device;
-extern struct platform_device jz4770_msc1_device;
-extern struct platform_device jz4770_msc2_device;
 extern struct platform_device jz4770_pcm_device;
-extern struct platform_device jz4770_pwm_device;
 extern struct platform_device jz4770_rtc_device;
 extern struct platform_device jz4770_usb_ohci_device;
 extern struct platform_device jz4770_usb_otg_device;
