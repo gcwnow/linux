8031676b794d8fdd45f706a861fde27f3aa0c0c5
Paul Cercueil <paul@crapouillou.net>
2015-05-31 13:39:52 +0200

    mmc: jz4740: Fix error handling in the probe function
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- drivers/mmc/host/jz4740_mmc.c
+++ drivers/mmc/host/jz4740_mmc.c
@@ -1055,23 +1055,25 @@ static int jz4740_mmc_probe(struct platform_device* pdev)
 			dev_name(&pdev->dev), host);
 	if (ret) {
 		dev_err(&pdev->dev, "Failed to request irq: %d\n", ret);
-		goto err_release_dma;
+		goto err_free_host;
 	}
 
 	jz4740_mmc_clock_disable(host);
 	setup_timer(&host->timeout_timer, jz4740_mmc_timeout,
 			(unsigned long)host);
 
-	host->use_dma = true;
-	if (host->use_dma && jz4740_mmc_acquire_dma_channels(host) != 0)
-		host->use_dma = false;
+	ret = jz4740_mmc_acquire_dma_channels(host);
+	if (ret == -EPROBE_DEFER)
+		goto err_free_irq;
+
+	host->use_dma = !ret;
 
 	platform_set_drvdata(pdev, host);
 	ret = mmc_add_host(mmc);
 
 	if (ret) {
 		dev_err(&pdev->dev, "Failed to add mmc host: %d\n", ret);
-		goto err_free_irq;
+		goto err_release_dma;
 	}
 	dev_info(&pdev->dev, "JZ SD/MMC card driver registered\n");
 
@@ -1081,11 +1083,11 @@ static int jz4740_mmc_probe(struct platform_device* pdev)
 
 	return 0;
 
-err_free_irq:
-	free_irq(host->irq, host);
 err_release_dma:
 	if (host->use_dma)
 		jz4740_mmc_release_dma_channels(host);
+err_free_irq:
+	free_irq(host->irq, host);
 err_free_host:
 	mmc_free_host(mmc);
 
