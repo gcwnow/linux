c87c8750fae37e0df70750dd7f2d1293e1b6b86c
Maarten ter Huurne <maarten@treewalker.org>
2015-06-26 09:43:10 +0200

    watchdog: jz4740: Mark as compatible with jz4770

--- drivers/watchdog/jz4740_wdt.c
+++ drivers/watchdog/jz4740_wdt.c
@@ -137,6 +137,7 @@ static const struct watchdog_ops jz4740_wdt_ops = {
 #ifdef CONFIG_OF
 static const struct of_device_id jz4740_wdt_of_matches[] = {
 	{ .compatible = "ingenic,jz4740-watchdog", },
+	{ .compatible = "ingenic,jz4770-watchdog", },
 	{ .compatible = "ingenic,jz4780-watchdog", },
 	{ /* sentinel */ }
 };
