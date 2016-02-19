f7395db2fdae655c4783bb39629452aa3cf53077
Maarten ter Huurne <maarten@treewalker.org>
2016-02-19 05:19:01 +0100

    MIPS: JZ4770: Simplified FPU detection override
    Rather than overriding __cpu_has_fpu() to return 1 always, just don't
    call it and always set the FPU options.

--- arch/mips/kernel/cpu-probe.c
+++ arch/mips/kernel/cpu-probe.c
@@ -54,13 +54,7 @@ static inline unsigned long cpu_get_fpu_id(void)
  */
 static inline int __cpu_has_fpu(void)
 {
-#if defined(CONFIG_MACH_JZ4770)
-	/* (cpu_get_fpu_id() & 0xff00) no use again in jz4760B */
-	/* so we have to force it to 1 */
-	return 1;
-#else
 	return (cpu_get_fpu_id() & FPIR_IMP_MASK) != FPIR_IMP_NONE;
-#endif
 }
 
 static inline unsigned long cpu_get_msa_id(void)
@@ -1512,11 +1506,11 @@ static inline void cpu_probe_ingenic(struct cpuinfo_mips *c, unsigned int cpu)
 #if defined(CONFIG_MACH_JZ4770)
 		c->isa_level = MIPS_CPU_ISA_M32R2;
 		c->tlbsize = 32;
+		/* FPIR ProcessorID is 0, so __cpu_has_fpu() would return 0. */
+		c->options |= MIPS_CPU_FPU | MIPS_CPU_32FPR;
 #endif
 		__cpu_name[cpu] = "Ingenic JZRISC";
 
-		if (__cpu_has_fpu())
-			c->options |= MIPS_CPU_FPU | MIPS_CPU_32FPR;
 		break;
 	default:
 		panic("Unknown Ingenic Processor ID!");
