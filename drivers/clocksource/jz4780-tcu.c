f370ac0b4b0e965288ad9e4509cd0ad09599c2e7
Maarten ter Huurne <maarten@treewalker.org>
2016-02-22 03:28:52 +0100

    clocksource: jz4780-tcu: Drop OST support
    OST support will be moved to a separate clocksource driver.

--- drivers/clocksource/jz4780-tcu.c
+++ drivers/clocksource/jz4780-tcu.c
@@ -14,19 +14,15 @@
 
 #include "jz47xx-tcu.h"
 
-static cycle_t jz4780_tcu_clocksource_read(struct clocksource *cs);
-
-static struct jz47xx_tcu_channel_desc jz4780_tcu_channels[16] = {
-	[0] = JZ47XX_TCU_CHANNEL(2, JZ47XX_TCU_CHANNEL_FIFO),
-	[1] = JZ47XX_TCU_CHANNEL(2, 0),
-	[2] = JZ47XX_TCU_CHANNEL(2, 0),
-	[3] = JZ47XX_TCU_CHANNEL(2, JZ47XX_TCU_CHANNEL_FIFO),
-	[4] = JZ47XX_TCU_CHANNEL(2, JZ47XX_TCU_CHANNEL_FIFO),
-	[5] = JZ47XX_TCU_CHANNEL(1, JZ47XX_TCU_CHANNEL_FIFO),
-	[6] = JZ47XX_TCU_CHANNEL(2, 0),
-	[7] = JZ47XX_TCU_CHANNEL(2, 0),
-
-	[15] = JZ47XX_TCU_CHANNEL(0, JZ47XX_TCU_CHANNEL_OST),
+static struct jz47xx_tcu_channel_desc jz4780_tcu_channels[8] = {
+	[0] = JZ47XX_TCU_CHANNEL(1, JZ47XX_TCU_CHANNEL_FIFO),
+	[1] = JZ47XX_TCU_CHANNEL(1, 0),
+	[2] = JZ47XX_TCU_CHANNEL(1, 0),
+	[3] = JZ47XX_TCU_CHANNEL(1, JZ47XX_TCU_CHANNEL_FIFO),
+	[4] = JZ47XX_TCU_CHANNEL(1, JZ47XX_TCU_CHANNEL_FIFO),
+	[5] = JZ47XX_TCU_CHANNEL(0, JZ47XX_TCU_CHANNEL_FIFO),
+	[6] = JZ47XX_TCU_CHANNEL(1, 0),
+	[7] = JZ47XX_TCU_CHANNEL(1, 0),
 };
 
 static struct jz47xx_tcu_desc jz4780_tcu_desc = {
@@ -34,50 +30,13 @@ static struct jz47xx_tcu_desc jz4780_tcu_desc = {
 	.num_channels = ARRAY_SIZE(jz4780_tcu_channels),
 };
 
-static struct {
-	struct clocksource cs;
-	struct jz47xx_tcu_channel *channel;
-} jz4780_tcu_clocksource = {
-	.cs = {
-		.name	= "jz4780-tcu-clocksource",
-		.rating	= 200,
-		.read	= jz4780_tcu_clocksource_read,
-		.mask	= CLOCKSOURCE_MASK(64),
-		.flags	= CLOCK_SOURCE_IS_CONTINUOUS,
-	},
-};
-
-static cycle_t jz4780_tcu_clocksource_read(struct clocksource *cs)
-{
-	return jz47xx_tcu_read_channel_count(jz4780_tcu_clocksource.channel);
-}
-
-static u64 notrace jz4780_tcu_sched_read(void)
-{
-	return jz47xx_tcu_read_channel_count(jz4780_tcu_clocksource.channel);
-}
-
 static void __init jz4780_tcu_init(struct device_node *np)
 {
 	struct jz47xx_tcu *tcu;
-	unsigned long rate;
 	int err;
 
 	tcu = jz47xx_tcu_init(&jz4780_tcu_desc, np);
 	BUG_ON(IS_ERR(tcu));
-
-	jz4780_tcu_clocksource.channel = jz47xx_tcu_req_channel(tcu, 15);
-	BUG_ON(IS_ERR(jz4780_tcu_clocksource.channel));
-
-	rate = jz47xx_tcu_get_channel_rate(jz4780_tcu_clocksource.channel);
-	pr_info("jz4780-tcu-clocksource: OST rate is %luHz\n", rate);
-	BUG_ON(!rate);
-
-	jz47xx_tcu_enable_channel(jz4780_tcu_clocksource.channel);
-
-	err = clocksource_register_hz(&jz4780_tcu_clocksource.cs, rate);
-	BUG_ON(err);
-	sched_clock_register(jz4780_tcu_sched_read, 64, rate);
 
 	/* For local clock events */
 	err = jz47xx_tcu_setup_cevt(tcu, 5);
