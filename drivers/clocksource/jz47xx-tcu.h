18c3c0bb294acc637ee01a22fb629ec6e5768ffc
Maarten ter Huurne <maarten@treewalker.org>
2016-02-24 17:36:23 +0100

    clocksource: jz4780-tcu: Remove channel flags
    With the OST split off, the only flag left was the one to indicate
    which channels support FIFO mode. Since FIFO mode is not implemented,
    I don't think this is useful information.

--- drivers/clocksource/jz47xx-tcu.h
+++ drivers/clocksource/jz47xx-tcu.h
@@ -17,14 +17,11 @@
 struct jz47xx_tcu_channel_desc {
 	u8 irq;
 	u8 present: 1;
-	unsigned flags;
-#define JZ47XX_TCU_CHANNEL_FIFO	(1 << 0)
 };
 
-#define JZ47XX_TCU_CHANNEL(_irq, _flags) {	\
+#define JZ47XX_TCU_CHANNEL(_irq) {		\
 	.irq = _irq,				\
 	.present = 1,				\
-	.flags = _flags,			\
 }
 
 struct jz47xx_tcu_desc {
