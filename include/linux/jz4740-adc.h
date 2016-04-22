f0f6a7d419df53d40d4a1c1470ed374e5d6a4a69
Maarten ter Huurne <maarten@treewalker.org>
2016-04-22 13:16:19 +0200

    mfd: jz4740-adc: Correct JZ_ADC_CONFIG_XYZ_MASK
    The XYZ field is two bits wide (bit 14:13).
    
    The JZ_ADC_CONFIG_XYZ_MASK macro isn't used anywhere at the moment,
    but if it's defined it might as well be correct.

--- include/linux/jz4740-adc.h
+++ include/linux/jz4740-adc.h
@@ -19,7 +19,7 @@ int jz4740_adc_set_config(struct device *dev, uint32_t mask, uint32_t val);
 #define JZ_ADC_CONFIG_EX_IN		BIT(30)
 #define JZ_ADC_CONFIG_DNUM_MASK		(0x7 << 16)
 #define JZ_ADC_CONFIG_DMA_ENABLE	BIT(15)
-#define JZ_ADC_CONFIG_XYZ_MASK		(0x2 << 13)
+#define JZ_ADC_CONFIG_XYZ_MASK		(0x3 << 13)
 #define JZ_ADC_CONFIG_SAMPLE_NUM_MASK	(0x7 << 10)
 #define JZ_ADC_CONFIG_CLKDIV_MASK	(0xf << 5)
 #define JZ_ADC_CONFIG_BAT_MB		BIT(4)
