fd24228d55a8cf6d047a79f1e7d9852fef2346c2
Paul Cercueil <paul@crapouillou.net>
2016-07-08 14:50:11 +0200

    clk: ingenic: jz4740-cgu: Move some suspend/resume platform code here
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- arch/mips/include/asm/mach-jz4740/clock.h
+++ arch/mips/include/asm/mach-jz4740/clock.h
@@ -15,16 +15,6 @@
 #ifndef __ASM_JZ4740_CLOCK_H__
 #define __ASM_JZ4740_CLOCK_H__
 
-enum jz4740_wait_mode {
-	JZ4740_WAIT_MODE_IDLE,
-	JZ4740_WAIT_MODE_SLEEP,
-};
-
-void jz4740_clock_set_wait_mode(enum jz4740_wait_mode mode);
-
-void jz4740_clock_suspend(void);
-void jz4740_clock_resume(void);
-
 void jz4740_clock_udc_enable_auto_suspend(void);
 void jz4740_clock_udc_disable_auto_suspend(void);
 
