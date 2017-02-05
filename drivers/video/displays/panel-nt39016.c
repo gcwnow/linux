0d98801d3ef1e6d97a77cde9b9a14b1d97a33acd
Maarten ter Huurne <maarten@treewalker.org>
2017-02-05 20:37:48 +0100

    video: panel: NT39016: Use regmap to write registers

--- drivers/video/displays/panel-nt39016.c
+++ drivers/video/displays/panel-nt39016.c
@@ -3,6 +3,7 @@
 #include <linux/device.h>
 #include <linux/gfp.h>
 #include <linux/gpio.h>
+#include <linux/regmap.h>
 
 #include <video/jzpanel.h>
 #include <video/panel-nt39016.h>
@@ -10,24 +11,26 @@
 
 struct nt39016 {
 	struct nt39016_platform_data *pdata;
+	struct regmap *regmap;
 };
 
-#define RV(reg, val) (((reg) << 10) | (1 << 9) | (val))
-static const u16 panel_data[] = {
+#define RV(REG, VAL) { .reg = (REG), .def = (VAL), .delay_us = 2 }
+static const struct reg_sequence nt39016_panel_regs[] = {
 	RV(0x00, 0x07), RV(0x01, 0x00), RV(0x02, 0x03), RV(0x03, 0xCC),
 	RV(0x04, 0x46), RV(0x05, 0x05), RV(0x06, 0x00), RV(0x07, 0x00),
 	RV(0x08, 0x08), RV(0x09, 0x40), RV(0x0A, 0x88), RV(0x0B, 0x88),
 	RV(0x0C, 0x20), RV(0x0D, 0x20), RV(0x0E, 0x67), RV(0x0F, 0xA4),
 	RV(0x10, 0x04), RV(0x11, 0x24), RV(0x12, 0x24), RV(0x20, 0x00),
 };
-static const u16 panel_standby = RV(0x00, 0x05);
 #undef RV
 
-static void nt39016_write_reg(struct nt39016_platform_data *pdata, u16 data)
+static int nt39016_reg_write(void *context, unsigned int reg, unsigned int val)
 {
+	struct nt39016 *panel = context;
+	struct nt39016_platform_data *pdata = panel->pdata;
+	unsigned int data = (reg << 10) | (1 << 9) | val;
 	int bit;
 
-	udelay(2);
 	gpio_direction_output(pdata->gpio_enable, 0);
 
 	for (bit = 15; bit >= 0; bit--) {
@@ -41,7 +44,32 @@ static void nt39016_write_reg(struct nt39016_platform_data *pdata, u16 data)
 	gpio_direction_output(pdata->gpio_enable, 1);
 
 	/* Note: Both clock and enable pin are left in inactive state (1). */
+
+	return 0;
 }
+
+static const struct regmap_range nt39016_regmap_no_ranges[] = {
+	regmap_reg_range(0x13, 0x1D),
+	regmap_reg_range(0x1F, 0x1F),
+};
+
+static const struct regmap_access_table nt39016_regmap_access_table = {
+	.no_ranges = nt39016_regmap_no_ranges,
+	.n_no_ranges = ARRAY_SIZE(nt39016_regmap_no_ranges),
+};
+
+static const struct regmap_config nt39016_regmap_config = {
+	.reg_bits = 6,
+	.val_bits = 8,
+
+	// TODO: All registers are readable, but we haven't implemented reading yet.
+	.reg_write = nt39016_reg_write,
+	.max_register = 0x20,
+	.wr_table = &nt39016_regmap_access_table,
+	//.rd_table = &nt39016_regmap_access_table,
+
+	.cache_type = REGCACHE_FLAT,
+};
 
 static int nt39016_panel_init(void **out_panel, struct device *dev,
 			      void *panel_pdata)
@@ -57,8 +85,6 @@ static int nt39016_panel_init(void **out_panel, struct device *dev,
 	}
 
 	panel->pdata = pdata;
-
-	*out_panel = panel;
 
 	/* Reserve GPIO pins. */
 
@@ -99,6 +125,13 @@ static int nt39016_panel_init(void **out_panel, struct device *dev,
 	gpio_direction_output(pdata->gpio_enable, 1);
 	gpio_direction_output(pdata->gpio_data,   0);
 
+	panel->regmap = devm_regmap_init(dev, NULL, panel,
+					 &nt39016_regmap_config);
+	if (IS_ERR(panel->regmap))
+		return PTR_ERR(panel->regmap);
+
+	*out_panel = panel;
+
 	return 0;
 }
 
@@ -109,23 +142,27 @@ static void nt39016_panel_exit(void *panel)
 static void nt39016_panel_enable(void *panel)
 {
 	struct nt39016_platform_data *pdata = ((struct nt39016 *)panel)->pdata;
-	int i;
+	struct regmap *regmap = ((struct nt39016 *)panel)->regmap;
+	int err;
 
 	/* Reset LCD panel. */
 	gpio_direction_output(pdata->gpio_reset, 0);
 	udelay(50);
 	gpio_direction_output(pdata->gpio_reset, 1);
+	udelay(2);
 
 	/* Init panel registers. */
-	for (i = 0; i < ARRAY_SIZE(panel_data); i++)
-		nt39016_write_reg(pdata, panel_data[i]);
+	err = regmap_multi_reg_write(regmap, nt39016_panel_regs,
+				     ARRAY_SIZE(nt39016_panel_regs));
+	if (err)
+		printk("nt39016_panel_enable failed: %d\n", err);
 }
 
 static void nt39016_panel_disable(void *panel)
 {
-	struct nt39016_platform_data *pdata = ((struct nt39016 *)panel)->pdata;
+	struct regmap *regmap = ((struct nt39016 *)panel)->regmap;
 
-	nt39016_write_reg(pdata, panel_standby);
+	regmap_write(regmap, 0x00, 0x05);
 }
 
 struct panel_ops nt39016_panel_ops = {
